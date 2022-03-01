94fa4d0f79bed2928c8eb960284364c43c9050ba
Sunil Pandey2 <sunilp2@chetu.com>
2022-03-01 21:00:08 +0530

    first

---
+++ Migrations/20220202101146_m3.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CascadingDropdown.Migrations
+{
+    public partial class m3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DropCities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DropCities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DropCities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DropStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DropStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DropCities_CountryId",
+                table: "DropCities",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DropCities");
+
+            migrationBuilder.DropTable(
+                name: "DropStates");
+        }
+    }
+}
---
+++ Migrations/20220202101357_m4.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CascadingDropdown.Migrations
+{
+    public partial class m4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DropCities_Countries_CountryId",
+                table: "DropCities");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryId",
+                table: "DropCities",
+                newName: "StateId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DropCities_CountryId",
+                table: "DropCities",
+                newName: "IX_DropCities_StateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DropCities_States_StateId",
+                table: "DropCities",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DropCities_States_StateId",
+                table: "DropCities");
+
+            migrationBuilder.RenameColumn(
+                name: "StateId",
+                table: "DropCities",
+                newName: "CountryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DropCities_StateId",
+                table: "DropCities",
+                newName: "IX_DropCities_CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DropCities_Countries_CountryId",
+                table: "DropCities",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
